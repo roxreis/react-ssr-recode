3b34ca6376af5eee6089b10d7a8fa2fceb856d13
roxreis <rodrigoassuncao12@gmail.com>
2021-02-12 20:32:17 -0300

    projeto finalizado

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,59 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous" />
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+        <title>FSELETRO</title>
       </Head>
+      <Container>
+            <Navbar expand="lg">
+                <Navbar.Brand href="/" to="/">
+                    <img src="./images/logo.png" alt="logo"  /> 
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse >
+                    <Nav className="d-flex justify-content-around w-100 bg-danger">
+                        <Nav.Item>
+                            <strong><Nav.Link  className="text-white" href="#" to="/produtos">PRODUTOS</Nav.Link></strong>
+                        </Nav.Item>
+                        <Nav.Item>
+                        <strong><Nav.Link  className="text-white" href="#" to="/contatos" >CONTATO</Nav.Link></strong>
+                        </Nav.Item>
+                        <Nav.Item>
+                        <strong><Nav.Link  className="text-white" href="#" to="/pedidos">PEDIDOS</Nav.Link></strong>
+                        </Nav.Item>
+                        <Nav.Item>
+                        <strong><Nav.Link  className="text-white" href="#" to="/lojas">LOJAS</Nav.Link></strong>
+                        </Nav.Item>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+          <section className={styles.sectionHome}>
+                <div className={styles.bannerHome}>
+                  <img className={styles.bannerHomeimg} src="./images/banner.jpg" alt="banner"  />
+                  <div className={styles.bannerHomediv}>
+                      <h1 className={styles.bannerHomeh1}>BEM VINDOS A NOSSA LOJA!</h1>
+                      <p>Aqui é barato mesmo!</p>
+                  </div>
+                </div>
+          </section>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+       
+        <footer className={styles.footer}>
+              <div className={styles.formasPgto}>
+                  <h4 >Formas de pagamento </h4>
+                  <img src='./images/formas-pgto.png' alt="formas de pagamento" />
+              </div>
+        </footer>
+      </Container>
     </div>
   )
 }
